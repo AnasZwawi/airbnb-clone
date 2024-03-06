321745b789596c4dad710c24b03bb5cc23bdb88d
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-06 19:58:39 +0100

    fixed a styling issue

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -111,13 +111,13 @@ async function HomeId({ params }: { params: { id: string } }) {
           className="rounded-lg h-full object-cover w-full"
         />
       </div>
-      <div className="flex gap-x-4 mt-2 mx-auto h-[75px] overflow-x-scroll no-scrollbar">
+      <div className="flex gap-x-4 mt-2 mx-auto h-fit overflow-x-scroll no-scrollbar">
         {data?.photos.map((photo: string, index: number) => (
           <div key={index} className="w-[150px] h-[75px] rounded-md ">
             <Image
               alt="Image of Home"
               src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
-              fill
+              
               className="rounded-md w-[150px] h-[75px]"
             />
           </div>
