627b64463b2cf404ca175de886ba72c93aafcfc3
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-07 17:07:56 +0100

    styling slider

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -127,7 +127,7 @@ async function HomeId({ params }: { params: { id: string } }) {
                 src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
                 className="h-full object-cover w-full"
               />
-              <div className="absolute top-0 left-0 w-full h-full z-20 hover:bg-black bg:bg-opacity-35 "/>
+              <div className="absolute top-0 left-0 w-full h-full z-20 hover:bg-black hover:bg-opacity-35 "/>
             </div>
           ))}
           
