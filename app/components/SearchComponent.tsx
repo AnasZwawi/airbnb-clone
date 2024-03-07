c30428a3bf89d5a9b2d35560695827e4fca383ce
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-07 22:25:38 +0100

    styling listing card

--- app/components/SearchComponent.tsx
+++ app/components/SearchComponent.tsx
@@ -55,7 +55,7 @@ export const SearchComponent = () => {
     <div>
       <Dialog>
         <DialogTrigger asChild>
-          <div className="shadow-[0_2px_5px_0px_rgba(0,0,0,0.1)] transition-shadow duration-200 hover:shadow-md mx-5 mt-2 rounded-full py-[5px] px-3 border flex justify-between items-center cursor-pointer">
+          <div className="shadow-[0_2px_5px_0px_rgba(0,0,0,0.1)] transition-shadow duration-200 hover:shadow-md mx-5 mt-2 rounded-full py-[7px] px-3 border flex justify-between items-center cursor-pointer">
             <div className="flex h-full divide-x font-medium text-sm sm:text-[16px] text-gray-700">
               <p className="px-2 sm:px-4">Anywhere</p>
               <p className="px-2 sm:px-4">Any Week</p>
