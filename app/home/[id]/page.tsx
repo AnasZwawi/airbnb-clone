f1707fc65fd9678b9b3bd4978f133904c8d59b35
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-11 00:18:37 +0100

    resetting home id

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -105,7 +105,7 @@ async function HomeId({ params }: { params: { id: string } }) {
   let endTime = new Date().getTime();
 
   return (
-    <div className="w-[85%] max-w-[1320px] lg:w-[75%] mx-auto mt-5">
+    <div className="w-[85%] max-w-[1320px] lg:w-[75%] mx-auto mt-5 relative">
       <div className="flex flex-col lg:flex-row justify-between gap-y-4 lg:items-center mb-4">
         <h1 className="font-semibold text-black text-[32px] tracking-tight lg:text-2xl">
           {data?.title}
