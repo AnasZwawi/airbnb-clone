afbede6478c7bbf946823f141281f87bc38d6cdc
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-11 02:10:59 +0100

    styling carousel

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -13,10 +13,10 @@ export const Gallery = ({ photos }: { photos: string[] | undefined }) => {
   return (
     <div className="fixed left-0 top-0 z-50 bg-black bg-opacity-90 backdrop-blur-md w-full h-[100vh] overflow-y-hidden flex justify-center items-center overflow-hidden">
       <Carousel>
-        <CarouselContent className="w-full h-full flext items-center justify-center">
+        <CarouselContent className="container h-full flext items-center justify-center">
           {photos?.map((photo: string, index: number) => (
             <CarouselItem key={index} className="w-[100%] h-auto">
-              <div className="w-[100%] flex flex-col items-center">
+              <div className="">
                 <Image
                   alt="Image of Home"
                   src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
