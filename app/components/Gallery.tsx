136a20cfc9421a17196a31961b8eaba7e132d214
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-11 02:33:05 +0100

    styling carousel

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -12,11 +12,11 @@ import Image from "next/image";
 export const Gallery = ({ photos }: { photos: string[] | undefined }) => {
   return (
     <div className="fixed left-0 top-0 z-50 bg-black bg-opacity-90 backdrop-blur-md w-full h-[100vh] overflow-y-hidden flex justify-center items-center overflow-hidden">
-      <Carousel>
-        <CarouselContent className="">
+      <Carousel className="w-fit p-x-4">
+        <CarouselContent className="w-fit">
           {photos?.map((photo: string, index: number) => (
             <CarouselItem key={index} className="">
-              <div className="">
+              <div className="w-fit">
                 <Image
                   alt="Image of Home"
                   src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
