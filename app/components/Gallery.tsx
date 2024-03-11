88a05e59b8015bff913f8b14883f25a2279ca0ab
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-11 01:25:45 +0100

    styling carousel

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -9,24 +9,22 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 
-
-export const Gallery = ({photos}: {photos: string[] | undefined}) => {
+export const Gallery = ({ photos }: { photos: string[] | undefined }) => {
   return (
     <div className="fixed left-0 top-0 z-50 bg-black bg-opacity-90 backdrop-blur-md w-full h-[100vh] overflow-y-hidden flex justify-center items-center overflow-hidden">
       <Carousel>
         <CarouselContent>
-        {photos?.map((photo: string, index: number) => (
+          {photos?.map((photo: string, index: number) => (
             <CarouselItem key={index}>
-            <div className="w-full h-full cursor-pointer">
-              <Image
-                alt="Image of Home"
-                src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
-                className="h-full object-cover w-full"
-              />
-            </div>
+              <div className="w-[90%] cursor-pointer">
+                <Image
+                  alt="Image of Home"
+                  src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
+                  className="w-full"
+                />
+              </div>
             </CarouselItem>
           ))}
-          
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
