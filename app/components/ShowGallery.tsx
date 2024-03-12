de35cb08e665414fd03d35d366905277ed871321
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-12 03:04:57 +0100

    styling carousel

--- app/components/ShowGallery.tsx
+++ app/components/ShowGallery.tsx
@@ -11,15 +11,18 @@ export const ShowGallery = ({ photos }: { photos: string[] | undefined }) => {
     return <div>No photos available</div>;
   }
 
-
-
   return (
     <>
       {gallery && <Gallery photos={photos} showGallery={showGallery} />}
       <div
         className={`relative flex flex-col md:flex-row gap-y-2 lg:gap-2 rounded-xl h-[550px] md:h-[450px] overflow-hidden`}
       >
-        <div className="relative w:full lg:w-1/2 h-full cursor-pointer">
+        <div
+          className="relative w:full lg:w-1/2 h-full cursor-pointer"
+          onClick={() => {
+            showGallery(true);
+          }}
+        >
           <Image
             alt="Image of Home"
             src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photos[0]}`}
@@ -30,7 +33,13 @@ export const ShowGallery = ({ photos }: { photos: string[] | undefined }) => {
         </div>
         <div className="w:full lg:w-1/2 h-full grid grid-cols-2 grid-rows-2 row-auto gap-2">
           {photos.slice(1, 5).map((photo: string, index: number) => (
-            <div key={index} className="relative w-full h-full cursor-pointer">
+            <div
+              key={index}
+              onClick={() => {
+                showGallery(true);
+              }}
+              className="relative w-full h-full cursor-pointer"
+            >
               <Image
                 alt="Image of Home"
                 src={`https://jxvqpjydezilbytxarzd.supabase.co/storage/v1/object/public/images/${photo}`}
