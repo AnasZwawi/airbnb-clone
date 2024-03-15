64fd3a4cc7288e57a20575d2611ba8c62f929731
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-15 02:32:55 +0100

    styled and added host section

--- app/home/[id]/components/HostInfo.tsx
+++ app/home/[id]/components/HostInfo.tsx
@@ -10,19 +10,19 @@ interface HostInfoProps {
 
 export const HostInfo: React.FC<HostInfoProps> = ({ profilePicture, hostName }) => {
   return (
-    <section className="bg-gray-400 md:bg-white p-8 md:p-y-8 md:px-0">
-      <h2 className="text-xl font-semibold text-black pb-4">Meet your Host</h2>
-      <div className="flex flex-col rounded-2xl bg-gray-200 p-0 md:p-8 w-full items-center md:flex-row gap-x-0 md:gap-x-6 gap-y-5">
-        <div className="bg-white flex flex-col justify-center w-[320px] h-fit p-4 rounded-2xl shadow-[0px_7px_25px_5px_#00000024]">
+    <section className="bg-muted-foreground md:bg-white p-8 md:p-y-8 md:px-0">
+      <h2 className="text-2xl font-semibold text-black pb-4">Meet your Host</h2>
+      <div className="flex flex-col rounded-2xl bg-muted-foreground p-0 md:p-8 w-full items-center md:flex-row gap-x-0 md:gap-x-8 gap-y-7">
+        <div className="bg-white flex flex-col items-center w-[320px] h-fit p-4 rounded-2xl shadow-[0px_7px_25px_5px_#00000024]">
           <img
             src={
               profilePicture ??
               "https://cdn-icons-png.flaticon.com/512/149/149071.png"
             }
             alt="User Profile image"
-            className="w-20 h-20 rounded-full"
+            className="w-28 h-28 rounded-full mb-2"
           />
-          <h3 className="text-center text-xl font-bold text-black">{hostName}</h3>
+          <h3 className="text-center text-[23px] font-semibold text-black">{hostName}</h3>
           <p className="text-center text-sm font-semibold text-black tracking-tighter">
             Host
           </p>
@@ -33,24 +33,24 @@ export const HostInfo: React.FC<HostInfoProps> = ({ profilePicture, hostName }) 
             Host details
           </h4>
           <p className="p-0 m-0 flex gap-x-1 items-center">
-            <Check className="w-4 h-4 text-gray-800" />
-            <span className="text-md text-gray-800">
+            <Check className="w-4 h-4 text-zinc-800" />
+            <span className="text-md text-zinc-800">
               confirmed email address
             </span>
           </p>
           <p className="p-0 m-0 flex items-center gap-x-1">
-            <Check className="w-4 h-4 text-gray-800" />
-            <span className="text-md text-gray-800 ">
+            <Check className="w-4 h-4 text-zinc-800" />
+            <span className="text-md text-zinc-800 ">
               confirmed phone number
             </span>
           </p>
-          <Button className="my-6 font-bold text-lg tracking-tight px-5 py-4 rounded-md bg-slate-900 text-white">
+          <Button className="my-6 font-semibold text-lg tracking-tight px-6 py-5 rounded-md bg-zinc-800 text-white">
             Contact Host
           </Button>
-          <Separator className="w-full bg-gray-400" />
+          <Separator className="w-full bg-zinc-300" />
           <div className="mt-4 flex gap-x-1 items-center">
             <ShieldAlert className="text-primary w-3 h-3" />
-            <p className="text-[13px] text-gray-700">
+            <p className="text-[13px] text-zinc-700">
               To protect your payment, never transfer money or communicate
               outside of the Tuniloge website.
             </p>
