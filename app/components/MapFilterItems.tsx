30694f9db9187b22077002ca1881af0c9577c75b
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-17 13:52:05 +0100

    refactored searching functionality

--- app/components/MapFilterItems.tsx
+++ app/components/MapFilterItems.tsx
@@ -1,52 +1,109 @@
-"use client";
-import React, { useCallback } from "react";
-import { categoryItems } from "../lib/categoryItems";
-import Link from "next/link";
-import Image from "next/image";
-import { useParams, usePathname, useSearchParams } from "next/navigation";
-import { create } from "domain";
-import { cn } from "@/lib/utils";
+/*   "use client";
+  import React, { useCallback } from "react";
+  import { categoryItems } from "../lib/categoryItems";
+  import Link from "next/link";
+  import Image from "next/image";
+  import { useParams, usePathname, useSearchParams } from "next/navigation";
+  import { create } from "domain";
+  import { cn } from "@/lib/utils";
 
-export const MapFilterItems = () => {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("filter");
-  const pathname = usePathname();
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString());
+  export const MapFilterItems = () => {
+    const searchParams = useSearchParams();
+    const search = searchParams.get("filter");
+    const pathname = usePathname();
+    const createQueryString = useCallback(
+      (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString());
 
-      params.set(name, value);
+        params.set(name, value);
 
-      return params.toString();
-    },
-    [searchParams]
-  );
-  return (
-    <div className="sticky z-40 border-b shadow-md bg-white bg-opacity-90 backdrop-blur-md top-[117px] sm:top-[76px]">
-      <div className="md:container flex w-[100%] z-40 gap-x-10 overflow-x-scroll no-scrollbar px-6">
-        {categoryItems.map((item) => (
-          <Link
-            key={item.id}
-            href={pathname + "?" + createQueryString("filter", item.name)}
-            className={cn(
-              search == item.name
-                ? "border-b-2 border-slate-600  flex-shrink-0"
-                : "opacity-70 flex-shrink-0",
-              "flex flex-col  hover:opacity-100 mx-auto py-3 gap-x-3 items-center transition-opacity"
-            )}
-          >
-            <div className="relative w-6 h-7">
-              <Image
-                src={item.imageUrl}
-                width={24}
-                height={24}
-                alt="Category Image"
-              />
-            </div>
-            <p className="text-xs font-semibold">{item.title}</p>
-          </Link>
-        ))}
+        return params.toString();
+      },
+      [searchParams]
+    );
+    return (
+      <div className="sticky z-40 border-b shadow-md bg-white bg-opacity-90 backdrop-blur-md top-[117px] sm:top-[76px]">
+        <div className="md:container flex w-[100%] z-40 gap-x-10 overflow-x-scroll no-scrollbar px-6">
+          {categoryItems.map((item) => (
+            <Link
+              key={item.id}
+              href={pathname + "?" + createQueryString("filter", item.name)}
+              className={cn(
+                search == item.name
+                  ? "border-b-2 border-slate-600  flex-shrink-0"
+                  : "opacity-70 flex-shrink-0",
+                "flex flex-col  hover:opacity-100 mx-auto py-3 gap-x-3 items-center transition-opacity"
+              )}
+            >
+              <div className="relative w-6 h-7">
+                <Image
+                  src={item.imageUrl}
+                  width={24}
+                  height={24}
+                  alt="Category Image"
+                />
+              </div>
+              <p className="text-xs font-semibold">{item.title}</p>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+ */
+  "use client"
+  import React, { useCallback, useMemo } from "react";
+  import { categoryItems } from "../lib/categoryItems";
+  import Link from "next/link";
+  import Image from "next/image";
+  import { useSearchParams, usePathname } from "next/navigation";
+  import { cn } from "@/lib/utils";
+  
+  
+  export const MapFilterItems = () => {
+    const searchParams = useSearchParams();
+    const search = searchParams.get("filter");
+    const pathname = usePathname();
+  
+    const createQueryString = useCallback(
+      (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set(name, value);
+        return params.toString();
+      },
+      [searchParams]
+    );
+  
+    // Memoize categoryItems array
+    const memoizedCategoryItems = useMemo(() => categoryItems, []);
+  
+    return (
+      <div className="sticky z-40 border-b shadow-md bg-white bg-opacity-90 backdrop-blur-md top-[117px] sm:top-[76px]">
+        <div className="md:container flex w-[100%] z-40 gap-x-10 overflow-x-scroll no-scrollbar px-6">
+          {memoizedCategoryItems.map((item) => (
+            <Link
+              key={item.id}
+              href={pathname + "?" + createQueryString("filter", item.name)}
+              className={cn(
+                search === item.name
+                  ? "border-b-2 border-slate-600  flex-shrink-0"
+                  : "opacity-70 flex-shrink-0",
+                "flex flex-col  hover:opacity-100 mx-auto py-3 gap-x-3 items-center transition-opacity"
+              )}
+            >
+              <div className="relative w-6 h-7">
+                <Image
+                  src={item.imageUrl}
+                  width={24}
+                  height={24}
+                  alt="Category Image"
+                />
+              </div>
+              <p className="text-xs font-semibold">{item.title}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
