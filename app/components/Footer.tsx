c486ae14d7358383b8c1119e5648e99e2ba9df26
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-17 18:18:45 +0100

    refactored searching functionality

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,29 +1,29 @@
-import { GithubIcon, TwitterIcon, YoutubeIcon } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import { GithubIcon, TwitterIcon, YoutubeIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export const Footer = () => {
   return (
-    
-      <div className="dark bg-gray-900 text-white py-8">
+    <div className="bg-stone-200 text-stone-900 py-8 absolute bottom-0">
       <div className="container mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center space-x-4">
-          <Link className="text-white hover:text-gray-300" href="#">
+          <Link className="text-stone-900 " href="#">
             <YoutubeIcon className="h-6 w-6" />
             <span className="sr-only">YouTube</span>
           </Link>
-          <Link className="text-white hover:text-gray-300" href="#">
+          <Link className="text-stone-900 " href="#">
             <GithubIcon className="h-6 w-6" />
             <span className="sr-only">GitHub</span>
           </Link>
-          <Link className="text-white hover:text-gray-300" href="#">
+          <Link className="text-stone-900 " href="#">
             <TwitterIcon className="h-6 w-6" />
             <span className="sr-only">Twitter</span>
           </Link>
         </div>
-        <p className="mt-4 md:mt-0 text-sm text-gray-300">© 2024 Acme Inc. All rights reserved.</p>
+        <p className="mt-4 md:mt-0 text-sm text-gray-300">
+          © 2024 Tuniloge Inc - LLC by Anas zouaoui. All rights reserved.
+        </p>
       </div>
     </div>
-  
-  )
-}
+  );
+};
