096152160ac44575ffc4163bf9ab45c0dc0d0363
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-17 18:20:19 +0100

    refactored searching functionality

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="bg-stone-200 text-stone-900 py-8 absolute bottom-0">
+    <div className="bg-stone-200 text-stone-900 py-8 absolute bottom-0 mt-6">
       <div className="container mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center space-x-4">
           <Link className="text-stone-900 " href="#">
