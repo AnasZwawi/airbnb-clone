e768bade809afe6ccdc757f26aaf5369c580a441
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-17 20:11:22 +0100

    refactored searching functionality

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -189,8 +189,8 @@ async function HomeId({ params }: { params: { id: string } }) {
                 (((endTime as number) - startTime) as number) /
                   (1000 * 3600 * 24)
               ) < 3 ? (
-                <div>
-                  <Star fill="black" className="w-4 h-4" /> New
+                <div className="flex gap-x-1">
+                  <Star fill="black" className="w-4 h-4" /> <p>New</p>
                 </div>
               ) : (
                 ""
