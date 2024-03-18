9c95b48d0bab5f186c56b3833e4a11d9d5bfd7e1
AnasZwawi <anaszouaoui17@gmail.com>
2024-03-18 11:23:53 +0100

    styling footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,9 +6,9 @@ export const Footer = () => {
     <div className="mt-auto w-full pt-12">
       <div className="bg-stone-100 border-t border-stone-300 text-stone-900 py-8 px-2 w-full">
         <div className="container mx-auto px-4 md:px-6 flex flex-col items-center">
-          <div className="w-full mx-0 md:mx-auto flex items-start flex-col md:flex-row md:gap-x-4 mb-3">
-            <Link className="hover:underline-offset-1" href={'/about'}>About us</Link>
-            <Link className="hover:underline-offset-1" href={'/contact'}>Contact us</Link>
+          <div className="w-full mx-0 md:mx-auto md:w-fitt flex items-start flex-col md:flex-row md:gap-x-4 mb-3">
+            <Link className="p-0 hover:border-b text-stone-900" href={'/about'}>About us</Link>
+            <Link className="p-0 hover:border-b text-stone-900" href={'/contact'}>Contact us</Link>
           </div>
           <p className="mt-4 md:mt-0 text-sm text-stone-900 text-center">
             © 2024 Tuniloge Inc - Created by Anas Zouaoui. All rights reserved.
